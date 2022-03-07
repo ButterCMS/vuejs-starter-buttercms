6b940f61d3db956566b31d71daae392652989e62
xgerzicakt <fAtdad-6pozja-byxnom>
2022-03-07 21:16:49 +0100

    ButterCMS sdk added

---
+++ src/utils/ButterCMS.ts
@@ -0,0 +1,10 @@
+import Butter, {ButterStatic} from "buttercms";
+
+export let butterCMS: ButterStatic | undefined = undefined;
+try {
+  const apiKey = String(import.meta.env.VITE_APP_BUTTER_CMS_API_KEY)
+  const preview = import.meta.env.VITE_APP_BUTTER_CMS_PREVIEW
+  butterCMS = Butter(apiKey, !preview);
+} catch (error) {
+  console.error(error)
+}
